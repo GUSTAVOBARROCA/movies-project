8a67e547c565f8c3718f89be6cce1d8cd9e18521
GUSTAVOBARROCA <gustavobarroca@hotmail.com>
2023-08-14 20:59:23 -0300

    estilos globais

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import GlobalStyle from './styles/globalStyles'
 import Router from './routes/routes.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyle/>
     <BrowserRouter>
       <Router/>
     </BrowserRouter>
